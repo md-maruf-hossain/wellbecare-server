98035d9942a69e28c1186fa01c069841c9cf15d7
marufhossain6470 <104669410+marufhossain6470@users.noreply.github.com>
2022-12-12 02:47:24 +0600

    appointment adding api created

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -10,36 +10,52 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3s0wivp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
-        const appointmentOptionCollection = client.db("wellBeCare").collection("appointmentOptions");
-        const bookingsCollection = client.db("wellBeCare").collection("bookings");
+async function run() {
+  try {
+    const appointmentOptionCollection = client.db("wellBeCare").collection("appointmentOptions");
+    const bookingsCollection = client.db("wellBeCare").collection("bookings");
 
-        app.get("/appointmentOptions", async (req, res)=>{
-            const query = {};
-            const options = await appointmentOptionCollection.find(query).toArray();
-            res.send(options);
-        });
+    app.get("/appointmentOptions", async (req, res) => {
+      const date = req.query.date;
+      const query = {};
+      const options = await appointmentOptionCollection.find(query).toArray();
 
-        app.post('/bookings', async (req, res)=>{
-            const booking = req.body;
-            const result = await bookingsCollection.insertOne(booking);
-            res.send(result);
-        })
-    }
-    finally{}
+      const bookingQuery = { appointmentDate: date };
+      const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+
+
+      options.forEach((option) => {
+        const optionBooked = alreadyBooked.filter((book) => book.treatment === option.name);
+        const bookedSlots = optionBooked.map((book) => book.slot);
+        const remainingSlots = option.slots.filter((slot) => !bookedSlots.includes(slot));
+        option.slots = remainingSlots;
+      });
+      res.send(options);
+    });
+
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        appointmentDate: booking.appointmentDate,
+        email: booking.email,
+        treatment: booking.treatment,
+      }
+      const alreadyBooked = await bookingsCollection.find(query).toArray();
+      if(alreadyBooked.length){
+        const message = `You already have a booking on ${booking.appointmentDate}`
+        return res.send({acknowledged: false, message});
+      }
+
+      const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    });
+  } finally {
+  }
 }
-run().catch(console.log)
-
-
-
-
-
+run().catch(console.log);
 
 app.get("/", async (req, res) => {
   res.send("wellbe care server is running");
